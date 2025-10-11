3bd85c4ff3adaa550e7b845042ebf31b5651d1b8
Jobinsajan <jobinsajan16@gmail.com>
2025-10-11 22:21:16 +0530

    changes

--- campusmeet/settings.py
+++ campusmeet/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['172.20.4.208', 'localhost', '127.0.0.1','campusmeet-j6qc.onren
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +141,7 @@ CHANNEL_LAYERS = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
